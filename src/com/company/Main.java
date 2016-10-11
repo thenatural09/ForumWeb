8a6e1dfc5ff405c43335c95f7b527063ef01474f
Troy <tward4@tulane.edu>
2016-10-11 12:04:24 -0400

    Added input and login system

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -25,17 +26,65 @@ public class Main {
         Spark.get(
                 "/",
                 (request, response) -> {
+                    String replyId = request.queryParams("replyId");
+                    int idNum = -1;
+                    if (replyId != null) {
+                        idNum = Integer.valueOf(replyId);
+                    }
+                    Session session = request.session();
+                    String name = session.attribute("loginName");
+
+
                     HashMap m = new HashMap();
                     ArrayList<Message> msgs = new ArrayList<>();
                     for (Message message : messages) {
-                        if (message.replyId == -1) {
+                        if (message.replyId == idNum) {
                             msgs.add(message);
                         }
                     }
                     m.put("messages",msgs);
+                    m.put("name",name);
+                    m.put("replyId",idNum);
                     return new ModelAndView(m, "home.html");
                 },
                 new MustacheTemplateEngine()
         );
+
+        Spark.post(
+                "/login",
+                (request,response) -> {
+                    String name = request.queryParams("loginName");
+                    String password = request.queryParams("password");
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name,password);
+                        users.put(name,user);
+                    }
+                    else if(!password.equals(user.password)) {
+                        Spark.halt(403);
+                        return null;
+                    }
+                    Session session = request.session();
+                    session.attribute("loginName",name);
+                    response.redirect("/");
+                    return null;
+                }
+
+        );
+
+        Spark.post(
+                "/create-message",
+                (request, response) -> {
+                    String text = request.queryParams("text");
+                    int replyId = Integer.valueOf(request.queryParams("replyId"));
+                    Session session = request.session();
+                    String name = session.attribute("loginName");
+                    Message message = new Message(messages.size(),replyId,name,text);
+                    messages.add(message);
+                    response.redirect("/");
+                    return null;
+                }
+        );
+
     }
 }
